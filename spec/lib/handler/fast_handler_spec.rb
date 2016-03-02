03308ee5de55004d18fc476355380fa57b5feb4c
jack-nie <hust.ud.hui@gmail.com>
2016-03-03 00:52:36 +0800

    fix spelling mistakes

--- spec/lib/handler/fast_handler_spec.rb
+++ spec/lib/handler/fast_handler_spec.rb
@@ -2,12 +2,12 @@ require_relative "../../spec_helper"
 
 RSpec.describe FastHandler do
   describe "#consistant?" do
-   context "the phone lsit includes a number which is anothr number's prefix" do
+    context "the phone list includes a number which is another number's prefix" do
       let(:path) { File.join SPEC_ROOT, 'assets', '1_phone_list.txt' }
       let(:reader) { TxtReader.new path }
       subject { FastHandler.new reader, Trie.new }
 
-      it "returns false wnen the phone lsit includes a number which is anothr number's prefix" do
+      it "returns false wnen the phone list includes a number which is another number's prefix" do
         expect(subject.consistant?).to be_falsey
       end
     end
